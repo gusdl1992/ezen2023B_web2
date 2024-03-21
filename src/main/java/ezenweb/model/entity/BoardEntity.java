bfcbbc061ad47b35a9a1af462b92baa1cf95f8c1
gusdl1992 <tlgusdl92@gmail.com>
2024-03-21 18:47:47 +0900

    20240321

--- src/main/java/ezenweb/model/entity/BoardEntity.java
+++ src/main/java/ezenweb/model/entity/BoardEntity.java
@@ -1,25 +1,42 @@
 package ezenweb.model.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Entity // 해당 클래스와 연동DB 내 테이블과 매핑
+@Table(name = "board")
 @Builder
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class BoardEntity {  // 테이블
-    @Id
-    @Column(name = "게시물번호")
-    private int bno;            // 게시물 번호
+    @Id // PK
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Column(name = "게시물번호" , nullable = false)
+    private int bno;            // 게시물 번호 PK
     @Column(columnDefinition = "text" , name = "제목") // DB 타입을 변경 할수 있다.
     private String btitle;      // 제목
+
+    private  boolean 필드0;
+    private byte 필드1;
+    private short 필드2;
+    private  long 필드3;
+
+    private  char 필드4;
+    private double 필드5;
+    private  float 필드6;
+
+    private Date 필드7;
+    private LocalDateTime 필드8;
+
+
 
 }
 
